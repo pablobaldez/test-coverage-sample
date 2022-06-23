ad68d8e3a532ac809b8552e1a50d0e16e2a51982
Pablo <pablo.baldez@getmayd.com>
2022-06-23 15:14:01 +0200

    adding jacoco to the project

---
+++ mylibrary/src/main/java/com/mob1st/mylibrary/Calculator.kt
@@ -0,0 +1,18 @@
+package com.mob1st.mylibrary
+
+import java.lang.IllegalStateException
+
+data class Calculator(
+    val value1: Int,
+    val value2: Int
+) {
+
+    fun div(): Int {
+        return if (value1 > 0 && value2 > 0) {
+            value1 / value2
+        } else throw IllegalStateException("invalid check exception")
+    }
+
+
+
+}
